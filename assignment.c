9e2059bd535eba7a67e8a5c8909bdc45d140f0e9
GAMihov18 <23IMC10627@imc.ac.at>
2024-12-01 14:35:35 +0100

    Finish assignment

--- assignment.c
+++ assignment.c
@@ -10,9 +10,60 @@ int main(int argc, char *argv[]) {
     srand(time(NULL));
     int minrand = 1;
     int maxrand = 100;
+    size_t arraySize[2];
 
-    // WRITE YOUR CODE HERE
+    if (argc != 3)
+    {
+        printf("Incorrect usage. You provided n arguments. The correct number of arguments is 2\n");
+        return 1;
+    }
+    int converted;
+    for (size_t i = 1; i < (size_t)argc; i++)
+    {
+        converted = atoi(argv[i]);
+        if (converted<=0)
+        {
+            printf("Incorrect usage. The parameters you provided are not positive integers\n");
+            return 1;
+        }
+        arraySize[i-1] = converted;
+    }
+
+    int **array = (int **)malloc(arraySize[0] * sizeof(int *));
+    if (array == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
     
-
+    for (size_t i = 0; i < arraySize[0]; i++)
+    {
+        array[i] = (int *)malloc(arraySize[1] * sizeof(int));
+        if (array[i] == NULL)
+        {
+            printf("Memory allocation failed\n");
+            return 1;
+        }
+        for (size_t j = 0; j < arraySize[1]; j++)
+        {
+            array[i][j] = (rand() % (maxrand - minrand + 1)) + minrand;
+        }
+    }
+    FILE *file = fopen("matrix.txt", "w");
+    if (file == NULL) {
+        printf("Error: Could not open file.\n");
+        return 1;
+    }
+    for (size_t i = 0; i < arraySize[0]; i++)
+    {
+        for (size_t j = 0; j < arraySize[1]; j++)
+        {
+            fprintf(file, "%d ", array[i][j]);
+        }
+        fprintf(file, "\n");
+        free(array[i]);
+    }
+    free(array);
+    fclose(file);
     return 0;
 }
